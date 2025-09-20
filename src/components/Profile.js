999bb9076e510a9dd35b0879435eb3b9b0b19aca
jeremisoto2026 <jeremisoto2022@gmail.com>
2025-09-20 02:25:14 +0200

    Actualizar Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
 import { db } from "../services/database";
 import { doc, getDoc, collection, getDocs } from "firebase/firestore";
 
