9f1f79fa1e8ef4f5e54b215234d3f6f34398a3d9
jeremisoto2026 <jeremisoto2022@gmail.com>
2025-09-20 11:36:54 +0200

    Actualizar Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 import { Separator } from "@radix-ui/react-separator";
 import { getAuth } from "firebase/auth";
 import { getDoc, doc, collection, getDocs } from "firebase/firestore";
