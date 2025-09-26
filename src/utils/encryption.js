1839cb65bab033549fce40ef4667f107dfa34692
jeremisoto2026 <jeremisoto2022@gmail.com>
2025-09-26 23:53:16 +0200

    Delete src/utils/encryption.js

--- src/utils/encryption.js
+++
@@ -1,8 +0,0 @@
-// Encriptación básica para las API Keys
-export const encrypt = (text) => {
-  return btoa(text); // Encriptación Base64 simple
-};
-
-export const decrypt = (encryptedText) => {
-  return atob(encryptedText); // Desencriptación Base64
-};
